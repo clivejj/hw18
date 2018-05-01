a951daf7c80edc57339483d0d0a857cf9e6c8913
clivejj <cjohnston1@stuy.edu>
2018-04-30 22:29:11 -0400

    done:

--- hw18.py
+++ hw18.py
@@ -1,14 +1,38 @@
-data = open('book.txt', 'r').read()
-#print data
-words = data.replace("\n", " ")
-words = words.replace("\r", " ")
-words = words.split(" ")
+def read():
+    data = open('book.txt', 'r').read()
+    #print data
+    data = data.replace("\n", " ").replace("\r", " ")
+    data = data.split(" ")
+    data = [x for x in data if x != ""]
+    #print data
+    return data
+
+words = read()
 
 
 test = ["h", "h", "b", "b", "h"]
-def frequency(x, word):
-    rtn = reduce(lambda a, b : a + 1 if b == word else a, x)
-    return rtn
+def frequency(listy, word):
+    return reduce(lambda a, b : a + 1 if b == word else a, listy, 0)
 
-print frequency(test, "h")
-print frequency(test, "b")
+print str(frequency(test, "h")) + "... should be 3"
+print str(frequency(test, "b")) + "... should be 2"
+print str(frequency(words, "the")) + "... should be very large"
+
+def frequencies(listy, words):
+    #This will return the sum of the frequency of each indivisual word
+    #I am not sure if this is what is desired
+    return reduce(lambda a, b: a + 1 if b in words else a, listy, 0)
+
+print str(frequencies(test, ["h", "b"])) + "... should be 5"
+print str(frequencies(test, ["h", "438"])) + "... should be 3"
+print str(frequencies(words, ["the", "wefqqq"])) + "... should be 315"
+
+
+def mode(listy):
+    d =  {word: frequency(listy, word) for word in list(set(listy))}
+    #return d
+    return reduce(lambda x , key : key if d[key] > d[x] else x, d)
+
+print str(mode(test)) + "... should be h"
+print str(mode(words)) + "... should be some really basic word"
+
